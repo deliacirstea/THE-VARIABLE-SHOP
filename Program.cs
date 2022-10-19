9c3c3428f50e360899154976b768141fca3ad1b5
deliacirstea <deliutacirstea@gmail.com>
2022-10-19 18:44:19 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿
+string y = "sixth";
+Console.WriteLine("Welcome to my " + y + " challenge!\n");
+Console.WriteLine("--------------------------");
+Console.WriteLine("Example of all the variables disscused in the book: \n");
+
+
+int a = 15;
+Console.WriteLine(a);
+
+uint b = 32;
+Console.WriteLine(b);
+
+short c = 8;
+Console.WriteLine(c);
+
+ushort d = 9;
+Console.WriteLine(d);
+
+long e = 3362;
+Console.WriteLine(e);
+
+ulong f = 32548;
+Console.WriteLine(f);
+
+byte g = 85;
+Console.WriteLine(g);
+
+sbyte h = 36;
+Console.WriteLine(h);
+
+char i = 'A';
+Console.WriteLine(i);
+
+string j = "sunny days";
+Console.WriteLine(j);
+
+bool k = true;
+Console.WriteLine(k);
+
+float l = 4.0f;
+Console.WriteLine(l);
+
+double m = 5.6;
+Console.WriteLine(m);
+
+decimal n = 30;
+Console.WriteLine(n);
+
+
